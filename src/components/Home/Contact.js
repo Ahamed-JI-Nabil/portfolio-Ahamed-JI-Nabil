f3b6a2b851b683079fbfa6c97dd08347afe4a6ac
NABIL <johayeriqbal@gmail.com>
2022-06-09 23:07:27 +0600

    contact me form added and mail sendig working

---
+++ src/components/Home/Contact.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className='flex items-center justify-center lg:pb-32 pb-28' id='contact'>
+
+            <div className="card max-w-max lg:max-w-2xl bg-base-100 shadow-xl text-teal-200 text-center">
+                <div className="card-body">
+                    <h2 className="text-4xl mb-4">Get In Touch</h2>
+                    <p className='mb-6'>I'm actively currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi don't hesitate, I'll try my best to get back to you!</p>
+                    <form
+                        action="https://formspree.io/f/xjvlzroe"
+                        method="POST"
+                    >
+                        <div className='mb-4'>
+                            <input type="text" placeholder="Your name" name="name" className="input w-full max-w-md mb-4" />
+
+                            <textarea type="text" placeholder="Your Massage" name="message" className="textarea textarea-bordered w-full max-w-md" />
+                        </div>
+                        <button className='btn px-12' type="submit">Send</button>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Contact;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import About from './About';
+import Contact from './Contact';
 import Intro from './Intro';
 import Skills from './Skills';
 
@@ -9,6 +10,7 @@ const Home = () => {
             <Intro></Intro>
             <About></About>
             <Skills></Skills>
+            <Contact></Contact>
         </div>
     );
 };
